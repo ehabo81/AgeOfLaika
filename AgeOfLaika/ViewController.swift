119629f0e1fc785f1b70636be71bd97771b34cf7
ehabo81 <ehabo81@gmail.com>
2015-05-30 15:26:34 +0300

    Initial Commit

---
+++ AgeOfLaika/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AgeOfLaika
+//
+//  Created by Ehab Issa on 5/30/15.
+//  Copyright (c) 2015 Ehab Issa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
